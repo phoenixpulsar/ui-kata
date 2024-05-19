373bce33e0d9650f6aa382387b547062f64ba96b
Phoenix Pulsar <phoenixpulsar@protonmail.com>
2024-05-18 21:18:02 -0600

    Added firebase

---
+++ firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+console.log("Init Firebase");
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDJpBzUtnaft54owPcdzxxoAdX15YMOrgk",
+  authDomain: "ordo-one.firebaseapp.com",
+  projectId: "ordo-one",
+  storageBucket: "ordo-one.appspot.com",
+  messagingSenderId: "913175168226",
+  appId: "1:913175168226:web:afd288d5e2ca60a8cb0abb",
+  measurementId: "G-9GYS6CHQ0R",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
