3171175261100df5d4235659f4e52f83fa52172f
Phoenix Pulsar <phoenixpulsar@protonmail.com>
2024-06-18 17:49:10 -0600

    Wip images

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,6 @@
 import { defineConfig } from "vite";
-import staticCopy from "vite-plugin-static-copy";
 
 export default defineConfig({
-  plugins: [
-    staticCopy({
-      targets: [
-        {
-          src: "src/assets/*",
-          dest: "assets/",
-        },
-      ],
-    }),
-  ],
   base: "/",
   build: {
     outDir: "dist",
