f32ce7883cba87039077b412c269b397576b91a9
Phoenix Pulsar <phoenixpulsar@protonmail.com>
2024-06-18 17:42:25 -0600

    WIP assets, include everything, add / infront of link of icons

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
 import { defineConfig } from "vite";
+import staticCopy from "vite-plugin-static-copy";
 
 export default defineConfig({
+  plugins: [
+    staticCopy({
+      targets: [
+        {
+          src: "src/assets/*",
+          dest: "assets/",
+        },
+      ],
+    }),
+  ],
   base: "/",
   build: {
     outDir: "dist",
